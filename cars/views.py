c1d46ec0c9819aa9405a656b7f908c77649143d1
Juliano-Formigari <julianomix@gmail.com>
2024-04-08 22:40:31 -0300

    desenvolvendo apps, configurando urls e models

---
+++ cars/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from cars.models import Car
+
+
+def cars_view(request):
+    cars = Car.objects.all()
+
+    return render(request, 'cars.html', {'cars': cars})
