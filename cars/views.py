49acfab68456671aaf5aec2f95a0c7fc155cc7ce
Juliano-Formigari <julianomix@gmail.com>
2024-05-25 11:49:12 -0300

    refatorando para CBV

--- cars/views.py
+++ cars/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
+from django.views.generic import ListView
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarsView(View):
 
-    if search:
-        cars = cars.filter(model__icontains=search)
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
 
-    return render(request, 'cars.html', {'cars': cars})
+        if search:
+            cars = cars.filter(model__icontais=search)
+        
+        return render(request, 'cars.html', {'cars': cars})
 
 
-def new_car_view(request):
-    if request.method == 'POST':
+class CarsListView(ListView):
+    pass
+
+
+class NewCarView(View):
+
+    def get(self,request):
+        new_car_form = CarModelForm
+        return render(request, 'new_car.html', {'new_car_view': new_car_form})
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+            return redirect('car_list')
+        return render(request, 'new_car.html', {'new_car_view': new_car_form})
